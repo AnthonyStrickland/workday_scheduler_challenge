f8fbe35ac2ff3b3f3cd4bbe814d1547452800fdc
Anthony Strickland <anthonystrickland26@gmail.com>
2023-09-24 14:42:51 -0500

    updated readme, added comments

--- Asset/script.js
+++ Asset/script.js
@@ -3,12 +3,7 @@
 // in the html.
 $(function() {
 
-  // Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  // listener for click events on the save button. 
 
   $(".saveBtn").on("click", function(){
      
@@ -17,14 +12,10 @@ $(function() {
     
     localStorage.setItem(hour, message);
   });
-
   
   // Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
+  // block by comparing the id to the current hour. 
+  
     $(".time").each(function (){
       var hours =(parseInt($(this).attr("id").split("-")[1]));
       var now = dayjs().hour();
@@ -44,8 +35,7 @@ $(function() {
     }
     });   
   // Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+  // the values of the corresponding textarea elements. 
 
   $("#hour-9 .description").val(localStorage.getItem("hour-9"));
   $("#hour-10 .description").val(localStorage.getItem("hour-10"));
@@ -55,8 +45,8 @@ $(function() {
   $("#hour-14 .description").val(localStorage.getItem("hour-14"));
   $("#hour-15 .description").val(localStorage.getItem("hour-15"));
   $("#hour-16 .description").val(localStorage.getItem("hour-16"));
-  $("#hour-21 .description").val(localStorage.getItem("hour-21"));
-  //
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+  
   // Add code to display the current date in the header of the page.
   var today = dayjs();
   $('#currentDay').text(today.format('ddd, MMM DD, YYYY h:mm A'));
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # workday_scheduler_challenge
+
+## Description
+
+module 5 is my work day scheduler.  The object was to make the past grey the present red and the future events green.  Beside eache event is a save button where you can save your data that you typed up and be able to store the data until your next use of the program
+
+## Usage
+
+![Alt text](<Work Day Scheduler.png>)
+
+
+## Credits
+https://www.w3schools.com/jsref/jsref_split.asp
+
+https://day.js.org/docs/en/display/format
+
+https://github.com/kittykatt210?tab=repositories
+
+UTSA Bootcamp
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt
